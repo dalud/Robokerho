a7cb7da309f33ce3619847d919550f16614a0304
dalud <jaakko.niska@gmail.com>
2021-05-31 19:18:29 +0300

    modified arduino to move servo according to serial input

--- arduino/marina/marina.ino
+++ arduino/marina/marina.ino
@@ -1,17 +1,20 @@
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
+#include <Servo.h>
 
 String command;
 boolean led;
+Servo myservo;
+// int pos = 0;
 
-LiquidCrystal lcd(2,3,4,5,6,7);
+// LiquidCrystal lcd(2,3,4,5,6,7);
 
 
 void setup() {
- lcd.begin(16, 2);
- lcd.setCursor(0,0);
+ /*lcd.begin(16, 2);
+ lcd.setCursor(0,0);*/
  
  pinMode(LED_BUILTIN, OUTPUT);
- 
+ myservo.attach(9); 
  Serial.begin(9600);
 }
 
@@ -22,24 +25,24 @@ void loop(){
   if(command == "on") ledOn();
   if(command == "off") ledOff();
   if(command == "clr") {
-    lcd.clear();
+    // lcd.clear();
     command = "";
   }
   
-  sout(command);
+  sout(command.toInt());
   
-  lcd.setCursor(0,1);
-  // lcd.print(millis());  
+  // lcd.setCursor(0,1);
+  // lcd.print(millis());   
 }
 
-void sout(String message) {
-  lcd.setCursor(0,0);
-  if(message.length() > 16) {
-    String scd = message.substring(16);
-    lcd.print(message);
-    lcd.setCursor(0,1);
-    lcd.print(scd);
-  } else lcd.print(message);
+void sout(int pos) {
+  myservo.write(pos+90);
+  if(pos){
+    ledOn();
+  } else {
+    ledOff();
+  }
+  delay(15)
 }
 
 void ledOn() {
@@ -49,3 +52,14 @@ void ledOn() {
 void ledOff() {
   digitalWrite(LED_BUILTIN, LOW);
 }
+/*void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+                  // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}*/
