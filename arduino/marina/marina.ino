c99f119b26bb378901e33fad026a483aa0eff508
dalud <jaakko.niska@gmail.com>
2021-09-16 17:36:31 +0300

    son added accelStepper and modified marina.ino

--- arduino/marina/marina.ino
+++ arduino/marina/marina.ino
@@ -1,18 +1,27 @@
 #include <Servo.h>
+#include <AccelStepper.h>
 
 String command;
 
 Servo mouth;
 int scalar = 90; // 90 for continuous, 0 for standard 180*d
 
-int dly = 30; // Scale down to speed motor functions up
+AccelStepper shoulder_R(1, 8, 9);
+
+int dly = 1; // Universal delay. Scale down to speed motor functions up
+float pause = .3; // Audio amplitude interpreted as silence
 
 
 void setup() {
  mouth.attach(2);
  Serial.begin(9600);
- // zero motors
+ // zero mouth motors
  moveMouth('L', 0);
+
+ shoulder_R.setMaxSpeed(5000); //SPEED = Steps / second  
+ shoulder_R.setAcceleration(1000); //ACCELERATION = Steps /(second)^2    
+ shoulder_R.setSpeed(1500);
+ delay(500);
 }
 
 void loop(){
@@ -25,6 +34,10 @@ void loop(){
   if(cmd == "ml") {
     moveMouth('L', command.substring(2).toInt());
   }
+
+  if(command.substring(2).toInt() > pause) {
+    moveStepper(shoulder_R);
+  }  
   
   delay(dly);
 }
@@ -37,3 +50,9 @@ void moveMouth(char channel, int pos) {
   }
 }
 
+void moveStepper(AccelStepper motor) {
+  motor.enableOutputs();
+  motor.runSpeed();
+  // Serial.println(shoulder_R.currentPosition());
+  motor.disableOutputs();
+}
