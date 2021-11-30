9db1790a1489dfdf8c371931512d23c7f0b7075b
dalud <jaakko.niska@gmail.com>
2021-11-30 20:22:14 +0200

    Son added accelSteppep debugs

---
+++ arduino/accelStepper_debug/stepper_debug.ino
@@ -0,0 +1,16 @@
+int DIR = 2;
+int PUL = 3;
+
+int dly = 1000;
+
+void setup() {
+  pinMode(DIR, OUTPUT);
+  pinMode(PUL, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PUL, HIGH);
+  delayMicroseconds(dly);
+  digitalWrite(PUL, LOW);
+  delayMicroseconds(dly);
+}
