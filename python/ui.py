8af7b6c90356d8c14566d52b128d657581fad7b1
dalud <jaakko.niska@gmail.com>
2022-01-02 14:54:53 +0200

    4 modified ui

--- python/ui.py
+++ python/ui.py
@@ -18,16 +18,16 @@ def enqueue_output(out, queue):
     for line in iter(out.readline, ''):
         queue.put(line)
     out.close()
-    
+
 # Read config file
 parser = configparser.ConfigParser()
 parser.read('/home/pi/robokerho/config')
 
 # Build UI
 layout = [[ui.Text("Robohemian: "+parser.get('env', 'name'), font="arial 16 bold")],
-          [ui.Button("CONFIG", button_color="orange"), ui.Button("RUN", button_color="green"), ui.Button("STOP", button_color="brown"), ui.Button("EXIT")],
+          [ui.Button("CONFIG", button_color="orange"), ui.Button("RUN", button_color="green"), ui.Button("STOP", button_color="brown"), ui.Button("REBOOT")],
           [ui.Multiline(reroute_stdout=True, reroute_stderr=True, auto_refresh=True, autoscroll=True, expand_x=True, expand_y=True, no_scrollbar=True)]]
-window = ui.Window("Robohemian: "+parser.get('env', 'name'), layout, size=(420, 320), default_button_element_size=(11, 3), auto_size_buttons=False, resizable=True)
+window = ui.Window("Robohemian: "+parser.get('env', 'name'), layout, size=(420, 320), default_button_element_size=(11, 4), auto_size_buttons=False, resizable=True)
 
 # Helpers
 def start(cmd):
@@ -43,28 +43,36 @@ while True:
 
     if event == "CONFIG":
         start(['killall', 'python3'])
-        start(['python3', '/home/pi/robokerho/python/selectBT.py'])
+        if not process:
+            start(['python3', '/home/pi/robokerho/python/config.py'])
 
     if event == "RUN":
         start(['killall', 'python3'])
-        if (parser.get('env', 'robo') == 'veke'):
-            start(['python3', '/home/pi/robokerho/python/veke.py'])
-        else:
-            start(['python3', '/home/pi/robokerho/python/robohemian.py'])
+        if not process:
+            if (parser.get('env', 'robo') == 'veke'):
+                start(['python3', '/home/pi/robokerho/python/veke.py'])
+            else:
+                start(['python3', '/home/pi/robokerho/python/robohemian.py'])
 
     if event == "STOP":
         start(['killall', 'python3'])
         if process:
-            #process.stdout.close()
+            process.stdout.close()
             process.send_signal(signal.SIGTERM)
-            
-    if event == "EXIT" or event == ui.WIN_CLOSED:
+
+    if event == "REBOOT":
+        start(['sudo', 'reboot'])
+
+    if event == ui.WIN_CLOSED:
         start(['killall', 'python3'])
+        if process:
+            process.stdout.close()
+            process.send_signal(signal.SIGTERM)
         break
 
     try: line = q.get_nowait()
     except Empty:
         continue
     else: print(line)
-    
+
 window.close()
