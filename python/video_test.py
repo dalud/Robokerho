60d398a85224b3c1c708ac0799cc1c5653092ca4
dalud <jaakko.niska@gmail.com>
2022-03-12 16:45:57 +0200

    TV added video_test.py

---
+++ python/video_test.py
@@ -0,0 +1,5 @@
+import vlc
+#media = vlc.MediaPlayer("/home/pi/robokerho/samples/video/Unheroic_labours_2_h264.mov")
+media = vlc.MediaPlayer("/home/pi/robokerho/samples/video/sample_1280x720_surfing_with_audio.mp4")
+
+media.play()
