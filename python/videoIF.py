66f6a433c8e35e3e99f51f8295e3b41548dead36
dalud <jaakko.niska@gmail.com>
2022-03-14 19:31:40 +0200

    TV added videoIF, tv.py

---
+++ python/videoIF.py
@@ -0,0 +1,13 @@
+import vlc
+
+class Video:
+    def __init__(self, media):
+        self.media = vlc.MediaPlayer(media)
+        print("Media set:", media)
+
+    def play(self):
+        self.media.play()
+        self.media.toggle_fullscreen()
+
+    def time(self):
+        if self.media.is_playing(): return self.media.get_time()
--- python/video_test.py
+++ python/video_test.py
@@ -2,4 +2,8 @@ import vlc
 #media = vlc.MediaPlayer("/home/pi/robokerho/samples/video/Unheroic_labours_2_h264.mov")
 media = vlc.MediaPlayer("/home/pi/robokerho/samples/video/sample_1280x720_surfing_with_audio.mp4")
 
-media.play()
+
+
+while True:
+    media.play()
+    if media.is_playing(): print("TV:", media.get_time())
