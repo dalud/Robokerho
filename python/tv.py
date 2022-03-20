7e8dc9dd068ee7a36220292f993e9d0a66d0fe10
dalud <jaakko.niska@gmail.com>
2022-03-20 13:06:56 +0200

    TV modified multiple

--- python/tv.py
+++ python/tv.py
@@ -1,35 +1,24 @@
 from videoIF import Video
+from wlanIF import Wlan
+import sys
 
+
+# Helpers
+flush = sys.stdout.flush
+
+# Init video
 video = Video("/home/pi/robokerho/samples/video/sample_1280x720_surfing_with_audio.mp4")
-
 video.play()
+
+# Init Wlan
+wlan = Wlan()
+flush()
 
 while True:
     if video.time():
         print(video.time(), "(", int(video.time()/60000), ":", int(video.time()%60000/1000), ")")
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        wlan.broadcast('TV:' + str(video.time()))#str(robo.vekeActive(stream)))
+        flush()
 
 
 
