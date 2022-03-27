0fccef481ffef2a588644c00cf2759304275671b
dalud <jaakko.niska@gmail.com>
2022-03-27 18:44:58 +0300

    TV modified videoIF.py

--- python/videoIF.py
+++ python/videoIF.py
@@ -8,6 +8,7 @@ class Video:
     def play(self):
         self.media.play()
         self.media.toggle_fullscreen()
+        #self.media.set_position(.65)
 
     def time(self):
         if self.media.is_playing(): return self.media.get_time()
